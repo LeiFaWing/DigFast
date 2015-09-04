8f296751bbe1f0d5c9dfe8d7115f6bd909abec24
LeiFaWing <stevehlee@sbcglobal.net>
2015-09-03 20:02:33 -0700

    fixed hitboxes again

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -18,18 +18,34 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		playerMovement ();
+	}
+
+	void playerMovement() {
 		if (Input.GetKey (KeyCode.W)) {
 			transform.Translate (Vector3.up * speed * Time.deltaTime);
 			up = true;
+			down = false;
+			right = false;
+			left = false;
 		} else if (Input.GetKey (KeyCode.S)) {
 			transform.Translate (Vector3.down * speed * Time.deltaTime);
 			down = true;
+			up = false;
+			right = false;
+			left = false;
 		} else if (Input.GetKey (KeyCode.A)) {
 			transform.Translate (Vector3.left * speed * Time.deltaTime);
 			left = true;
+			down = false;
+			right = false;
+			up = false;
 		} else if (Input.GetKey (KeyCode.D)) {
 			transform.Translate (Vector3.right * speed * Time.deltaTime);
 			right = true;
+			left = false;
+			down = false;
+			up = false;
 		} else {
 			left = false;
 			right = false;
